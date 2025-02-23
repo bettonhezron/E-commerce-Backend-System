7713691be8da102d0fcd29e20dd6ff03d6a3bf80
bettonhezron <hezronbet@gmail.com>
2025-02-23 18:19:57 +0300

    Created  UserRegistrationDTO

--- src/main/java/com/hezron/ecommerce/dto/UserRegistration.java
+++ src/main/java/com/hezron/ecommerce/dto/UserRegistration.java
@@ -1,4 +1,20 @@
 package com.hezron.ecommerce.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class UserRegistration {
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
 }
