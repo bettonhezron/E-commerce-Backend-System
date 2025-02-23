88ac26c4ffda8469879c4627df63cd26c9275556
bettonhezron <hezronbet@gmail.com>
2025-02-23 12:01:19 +0300

    product entity

--- src/main/java/com/hezron/ecommerce/model/Product.java
+++ src/main/java/com/hezron/ecommerce/model/Product.java
@@ -1,4 +1,54 @@
 package com.hezron.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private  double price;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDate createdAt;
+
+    @Column(nullable = false)
+    private LocalDate updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDate.now();
+        updatedAt = LocalDate.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDate.now();
+    }
 }
