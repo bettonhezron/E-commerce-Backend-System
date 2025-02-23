1557504a299c3a89c5948a03442ea4b58537949a
bettonhezron <hezronbet@gmail.com>
2025-02-23 16:43:00 +0300

    created AddressRepo interface

---
+++ src/main/java/com/hezron/ecommerce/resitory/AddressRepository.java
@@ -0,0 +1,12 @@
+package com.hezron.ecommerce.resitory;
+
+import com.hezron.ecommerce.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findUserById(Long userId);
+    Optional<Address> findByUserIdAndIsDefaultTrue(Long userId);
+}
--- src/main/java/com/hezron/ecommerce/resitory/AddressRepostitory.java
+++
@@ -1,4 +0,0 @@
-package com.hezron.ecommerce.resitory;
-
-public interface AddressRepostitory {
-}
