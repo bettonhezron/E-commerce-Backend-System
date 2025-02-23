42fdd0380dc0290df651ffc370cbe9d0dfda1e0f
bettonhezron <hezronbet@gmail.com>
2025-02-23 15:04:37 +0300

    Created Cart entity

--- src/main/java/com/hezron/ecommerce/model/Cart.java
+++ src/main/java/com/hezron/ecommerce/model/Cart.java
@@ -1,12 +1,41 @@
 package com.hezron.ecommerce.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @Data
 @Entity
-@Table(name = "cart")
+@Table(name = "carts")
 public class Cart {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private Set<CartItem> cartItems;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
