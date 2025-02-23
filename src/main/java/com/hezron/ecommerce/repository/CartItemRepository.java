daf9f8719d1da46597096b525e206feb0e7247a6
bettonhezron <hezronbet@gmail.com>
2025-02-23 18:39:17 +0300

    Updated productRepo query

--- src/main/java/com/hezron/ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/hezron/ecommerce/repository/CartItemRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    List<CartItem> findByCardId(Long cartId);
+    List<CartItem> findByCartId(Long cartId);
     void deleteByCartId(Long cartId);
 }
--- src/main/java/com/hezron/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/hezron/ecommerce/repository/ProductRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findCategoryId(Long categoryId);
+    List<Product> findByCategoryId(Long categoryId);
     List<Product>  findByActiveTrue();
 
-    @Query("SELECT p FROM Product WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
     List<Product> search(String keyword);
 }
