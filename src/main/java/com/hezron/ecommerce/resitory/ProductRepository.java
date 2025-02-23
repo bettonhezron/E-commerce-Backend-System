612e0263ef914432be35eb0ae65f8e8a4fe98541
bettonhezron <hezronbet@gmail.com>
2025-02-23 16:48:28 +0300

    created ProductRepo interface

--- src/main/java/com/hezron/ecommerce/resitory/ProductRepository.java
+++ src/main/java/com/hezron/ecommerce/resitory/ProductRepository.java
@@ -1,4 +1,15 @@
 package com.hezron.ecommerce.resitory;
 
-public class ProductRepository {
+import com.hezron.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findCategoryId(Long categoryId);
+    List<Product>  findByActiveTrue();
+
+    @Query("SELECT p FROM Product WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+    List<Product> search(String keyword);
 }
