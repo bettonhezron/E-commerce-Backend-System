109885c30274c0f52646504f05f3db16757abe29
bettonhezron <hezronbet@gmail.com>
2025-02-23 18:16:50 +0300

    Created  UserDTO

--- src/main/java/com/hezron/ecommerce/dto/UserDTO.java
+++ src/main/java/com/hezron/ecommerce/dto/UserDTO.java
@@ -1,4 +1,12 @@
 package com.hezron.ecommerce.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    //no password for security
 }
