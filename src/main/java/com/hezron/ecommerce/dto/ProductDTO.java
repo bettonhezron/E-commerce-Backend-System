cbb8845e715fd8efdbc0f2035fd9ef2652f7f470
bettonhezron <hezronbet@gmail.com>
2025-02-23 18:27:43 +0300

    Created  ProductDTO

--- src/main/java/com/hezron/ecommerce/dto/ProductDTO.java
+++ src/main/java/com/hezron/ecommerce/dto/ProductDTO.java
@@ -1,4 +1,18 @@
 package com.hezron.ecommerce.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Data
 public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stockQuantity;
+    private Long categoryId;
+    private Set<String> imageUrls;
+    private boolean active;
 }
