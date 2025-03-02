c4c178e3504bcafad42122a90b8b27021121ef8e
bettonhezron <hezronbet@gmail.com>
2025-03-02 13:44:22 +0300

    updated UserRegDTO

--- src/main/java/com/hezron/ecommerce/dto/UserRegistrationDTO.java
+++ src/main/java/com/hezron/ecommerce/dto/UserRegistrationDTO.java
@@ -3,22 +3,35 @@ package com.hezron.ecommerce.dto;
 import com.hezron.ecommerce.model.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRegistrationDTO {
-    @NotBlank
-    @Email(message = "Invalid email format")
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
 
     @NotBlank(message = "Password is required")
     @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 
-
-    private String firstName;
-    private String lastName;
+    @Pattern(regexp = "^\\+?[0-9]*$", message = "Phone number should contain only digits and optional + prefix")
     private String phoneNumber;
-    private  Role role = Role.CUSTOMER;
+
+    private Role role;
 }
