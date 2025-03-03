f32b5243fe742cf56d8b2fd32a52b37b71c79b62
bettonhezron <hezronbet@gmail.com>
2025-03-03 19:07:36 +0300

    updated ResourceNotFoundException

--- src/main/java/com/hezron/ecommerce/exception/ResourceNotFoundException.java
+++ src/main/java/com/hezron/ecommerce/exception/ResourceNotFoundException.java
@@ -1,7 +1,31 @@
 package com.hezron.ecommerce.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Exception thrown when a requested resource is not found
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+
+    /**
+     * Constructs a new resource not found exception with a custom message
+     *
+     * @param message The custom error message
+     */
     public ResourceNotFoundException(String message) {
         super(message);
+    }
+
+    /**
+     * Constructs a new resource not found exception with the specified resource details
+     *
+     * @param resourceName Name of the resource (e.g. "Product")
+     * @param fieldName Field name used in the lookup (e.g. "id")
+     * @param fieldValue Value of the field that was not found
+     */
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
     }
 }
