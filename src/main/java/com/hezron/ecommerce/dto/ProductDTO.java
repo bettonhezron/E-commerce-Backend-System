1f0052470a91f1e5ca60e94cbf8aea4744ad4be4
bettonhezron <hezronbet@gmail.com>
2025-03-03 19:21:35 +0300

    Updated ProductDTO

--- src/main/java/com/hezron/ecommerce/dto/ProductDTO.java
+++ src/main/java/com/hezron/ecommerce/dto/ProductDTO.java
@@ -1,18 +1,47 @@
 package com.hezron.ecommerce.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+
 import java.math.BigDecimal;
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Data
 public class ProductDTO {
+
     private Long id;
+
+    @NotBlank(message = "Product name is required")
+    @Size(min = 2, max = 25, message = "Name must be between 2 and 255")
     private String name;
+
+    @Size(max = 2000, message = "Description cannot exceed 2000 characters")
     private String description;
+
+    @NotNull(message = "Price is required")
+    @Min(value = 0, message = "Price must be greater then 0")
     private BigDecimal price;
+
+    @NotNull(message = "Stock quantity is required")
+    @Min(value = 0, message = "Stock quantity must be greater then or equal to 0")
     private Integer stockQuantity;
+
     private Long categoryId;
-    private Set<String> imageUrls;
+
+    private String categoryName;
+
+    private List<String> imageUrls = new ArrayList<>();
+
     private boolean active;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime UpdatedAt;
 }
