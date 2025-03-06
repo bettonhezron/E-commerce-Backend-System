64aebd35c2e5d388a06a3a210c112ff12b58bc43
bettonhezron <hezronbet@gmail.com>
2025-03-06 08:23:32 +0300

    updated order repo and added order item repo

---
+++ src/main/java/com/hezron/ecommerce/repository/OrderItemRepository.java
@@ -0,0 +1,4 @@
+package com.hezron.ecommerce.repository;
+
+public class OrderItemRepository {
+}
--- src/main/java/com/hezron/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/hezron/ecommerce/repository/OrderRepository.java
@@ -2,11 +2,14 @@ package com.hezron.ecommerce.repository;
 
 import com.hezron.ecommerce.model.Order;
 import com.hezron.ecommerce.model.OrderStatus;
+import com.hezron.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
     List<Order> findByUserId(Long userId);
     List<Order> findByStatus(OrderStatus status);
+    List<Order> findByOrderByOrderDateDesc(User user);
 }
