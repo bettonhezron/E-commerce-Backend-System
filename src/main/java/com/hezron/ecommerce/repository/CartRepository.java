6e8b5770cbba65b0263f9b2377e1eed2cc4d2a24
bettonhezron <hezronbet@gmail.com>
2025-03-06 08:19:46 +0300

    updated cart repository

--- src/main/java/com/hezron/ecommerce/repository/CartRepository.java
+++ src/main/java/com/hezron/ecommerce/repository/CartRepository.java
@@ -1,10 +1,12 @@
 package com.hezron.ecommerce.repository;
 
 import com.hezron.ecommerce.model.Cart;
+import com.hezron.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart,Long> {
-    Optional<Cart> findByUserId(Long userId);
+    Optional<Cart> findByUser(User user);
+    Optional<Cart> findBySessionId(String sessionId);
 }
