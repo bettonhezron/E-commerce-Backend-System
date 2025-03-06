312069fbfc207ad9bc9d3998066a45879a251356
bettonhezron <hezronbet@gmail.com>
2025-03-06 08:02:36 +0300

    update OrderDTO and OrderItemDTO

--- src/main/java/com/hezron/ecommerce/dto/OrderDTO.java
+++ src/main/java/com/hezron/ecommerce/dto/OrderDTO.java
@@ -1,18 +1,30 @@
 package com.hezron.ecommerce.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDTO {
     private Long id;
-    private BigDecimal totalAmount;
+    private String orderNumber;
+    private LocalDateTime orderDate;
     private String status;
-    private Long shippingAddressId;
+    private BigDecimal subtotal;
+    private BigDecimal tax;
+    private BigDecimal shippingCost;
+    private BigDecimal totalAmount;
+    private String shippingAddress;
+    private String billingAddress;
     private  String trackingNumber;
     private LocalDateTime createdAt;
     private List<OrderItemDTO> items;
