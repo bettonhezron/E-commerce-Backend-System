fcf5ec89d876501a4b603315a84a3cb6888339da
bettonhezron <hezronbet@gmail.com>
2025-03-06 08:43:39 +0300

    Added cart service interface

--- src/main/java/com/hezron/ecommerce/service/CartService.java
+++ src/main/java/com/hezron/ecommerce/service/CartService.java
@@ -1,6 +1,18 @@
 package com.hezron.ecommerce.service;
 
+import com.hezron.ecommerce.dto.CartDTO;
+
 public interface CartService {
+
+    CartDTO getCurrentCart();
+
+    CartDTO addToCart(Long productId, Integer quantity);
+
+    CartDTO updateCartItem(Long itemId, Integer quantity);
+
+    CartDTO removeFromCart(Long itemId);
+
+    void clearCart();
 
 
 }
