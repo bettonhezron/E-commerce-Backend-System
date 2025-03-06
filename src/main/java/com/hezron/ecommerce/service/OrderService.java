2e007bab5d6bbe7f1ecc199b827aa24e96719243
bettonhezron <hezronbet@gmail.com>
2025-03-06 08:45:54 +0300

    Added order service interface

--- src/main/java/com/hezron/ecommerce/service/OrderService.java
+++ src/main/java/com/hezron/ecommerce/service/OrderService.java
@@ -1,4 +1,15 @@
 package com.hezron.ecommerce.service;
 
-public class OrderService {
+import com.hezron.ecommerce.dto.OrderDTO;
+import com.hezron.ecommerce.dto.OrderRequestDTO;
+
+import java.util.List;
+
+public interface OrderService {
+
+    OrderDTO placeOrder(OrderRequestDTO orderRequest);
+
+    OrderDTO getOrderById(Long id);
+
+    List<OrderDTO> getCurrentUserOrders();
 }
