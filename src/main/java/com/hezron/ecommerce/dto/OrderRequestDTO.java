1b1a057e76c1519e9dc2e88da7d9ce2f2f0454e3
bettonhezron <hezronbet@gmail.com>
2025-03-06 11:20:43 +0300

    Updated OrderRequestDTO

--- src/main/java/com/hezron/ecommerce/dto/OrderRequestDTO.java
+++ src/main/java/com/hezron/ecommerce/dto/OrderRequestDTO.java
@@ -1,5 +1,6 @@
 package com.hezron.ecommerce.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderRequestDTO {
+    private Long shippingAddressId;
     private String shippingAddress;
+
+    private Long  billingAddressId;
     private String billingAddress;
+
+    @NotBlank(message = "Payment method is required")
     private String paymentMethod;
+
     private String paymentDetails;
     private String specialInstructions;
 }
