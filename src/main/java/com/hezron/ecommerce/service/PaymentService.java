95ac8fa7b73b1ee20734ebd872253f5c2758b6fc
bettonhezron <hezronbet@gmail.com>
2025-03-14 20:04:49 +0300

    Added payment service interface

--- src/main/java/com/hezron/ecommerce/service/PaymentService.java
+++ src/main/java/com/hezron/ecommerce/service/PaymentService.java
@@ -1,10 +1,14 @@
 package com.hezron.ecommerce.service;
 
+import com.hezron.ecommerce.dto.PaymentDTO;
+import com.hezron.ecommerce.dto.PaymentRequestDTO;
+import com.hezron.ecommerce.dto.PaymentResponseDTO;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PaymentService {
-    public void makePayment(){
+public interface PaymentService {
+    PaymentResponseDTO processPayment(PaymentRequestDTO paymentRequestDTO);
+    PaymentDTO getPaymentByOrderId(Long orderId);
+    PaymentDTO getPaymentByTransactionId(String transactionId);
 
-    }
 }
