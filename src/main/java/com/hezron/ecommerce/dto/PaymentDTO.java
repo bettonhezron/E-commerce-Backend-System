12572bfab2830784ab3546060fbf979ea729bda5
bettonhezron <hezronbet@gmail.com>
2025-03-14 11:00:34 +0300

    Added paymentDTO

---
+++ src/main/java/com/hezron/ecommerce/dto/PaymentDTO.java
@@ -0,0 +1,25 @@
+package com.hezron.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentDTO {
+private Long id;
+private String transactionID;
+private Long orderId;
+private BigDecimal amount;
+private String currency;
+private String paymentMethod;
+private  String status;
+private String createdAt;
+
+
+}
