32eb50bdda345eaddf33092c2528bf3b43e16419
bettonhezron <hezronbet@gmail.com>
2025-03-18 18:00:35 +0300

    Added Admin dashboard endpoint

---
+++ src/main/java/com/hezron/ecommerce/controller/AdminDashboardController.java
@@ -0,0 +1,21 @@
+package com.hezron.ecommerce.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for Admin Dashboard operations.
+ */
+@RestController
+@RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminDashboardController {
+
+    @GetMapping
+    public ResponseEntity<String> getAdminDashboard() {
+        return ResponseEntity.ok("Welcome to the Admin Dashboard!");
+    }
+}
